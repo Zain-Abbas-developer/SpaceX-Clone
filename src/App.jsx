d505d9daa864d2aeaaa966a31588227feae71d88
Zain-Abbas-developer <zainabbasllc2@gmail.com>
2025-10-24 11:34:37 -0700

    third section in progress

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div className='h-full bg-black/40 '>
       <Navbar/>
       <div className='relative w-full h-screen  bg-linear-to-t from-black to-transparent'>
-          <div className=' relative w-full h-screen  top-[-55px] z-[-1] '>
+          <div className=' relative w-full h-screen z-[-1] '>
               <video
                 className="absolute top-0 left-0 w-full h-full object-cover"
                 autoPlay
@@ -30,7 +30,7 @@ const App = () => {
               </button>  
          </div>
        </div>
-       <div className=' relative w-full h-200 text-[#f0f0fa] gap-6 bg-linear-to-b from-black to-transparent'>
+       <div className=' relative w-full h-[130vh] text-[#f0f0fa] gap-6 bg-linear-to-b from-black to-transparent'>
           <div className='h-screen flex ml-[70px]'>
               {/* content are here  */}
               <div className='basis-[60vw] content-center mt-[200px]'>
@@ -40,10 +40,10 @@ const App = () => {
                  <ArrowRight className='-ml-2.5 transition-transform duration-300 ease-in-out group-hover:translate-x-2' size={20} />
                 </button>
               </div>
-              <div className='relative w-1/2 h-screen overflow-hidden content-center'>
+              <div className='relative w-1/2 h-[120vh] border-0 outline-0 overflow-hidden content-center'>
                 {/* video container */}
                  <video
-                    className="absolute top-[50px] left-[-310px] w-full h-full object-cover"
+                    className="absolute top-0 left-[-330px] w-full h-[130vh] outline-0 border-0 content-center overflow-visible object-cover"
                     autoPlay
                     loop
                     muted
@@ -55,7 +55,17 @@ const App = () => {
               </div>
           </div>
        </div>
-       {/* content goes here and make it simple and more reliable  */}
+       {/* Third section start here */}
+       <div className=' relative w-full h-[120vh] bg-[url(./src/assets/images/section_3.jpg)] bg-cover bg-center'>
+          <div className='w-full h-[120vh] bg-linear-to-l from-[#0e0d0d]/90 to-transparent to-50%'></div>
+          <div className='absolute top-[100px] right-1 w-[57vw] h-[55vh] border-2 border-red-50'>
+            <h1 className='text-[3rem] font-din uppercase'>Revolutionizing space<br/> technology</h1>
+            <p>SpaceX’s Starship spacecraft and Super Heavy rocket is a fully reusable transportation system<br/> designed to carry both crew and cargo to Earth orbit, the Moon, Mars, and beyond.</p>
+            <button className='group flex w-[110px] h-[53px] place-items-center text-center justify-center text-xs gap-4 bg-black/50 rounded border-2 border-[#504646] mt-5 hover:bg-white uppercase hover:text-black hover:border-0 duration-150'>learn more
+                 <ArrowRight className='-ml-2.5 transition-transform duration-300 ease-in-out group-hover:translate-x-2' size={20} />
+                </button>
+          </div>
+       </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ export default {
     extend: {
       fontFamily: {
         // Add your custom font families here
-        'space-reg': ['D-DIN-REG', 'sans-serif'],
         'space': ['D-DIN', 'sans-serif'],          // SpaceX's primary font
         'din': ['D-DIN-Bold', 'sans-serif'],       // SpaceX's bold font
         'heading': ['D-DIN-Condensed', 'sans-serif'] // SpaceX's condensed font for headings
